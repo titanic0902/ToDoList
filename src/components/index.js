2ac2a26e16795ca4ac1b7700c723f058a6003962
Ishita <ajeets1900@gmail.com>
2024-03-04 00:20:46 +0530

    first commit

---
+++ src/components/index.js
@@ -0,0 +1,3 @@
+// to import ever file in this one file 
+import TodoForm from "./TodoForm";
+import Todoitem from "./Todoitem";
---
+++ src/context/index.js
@@ -0,0 +1,2 @@
+// we created this file beacuse we will import every function in this file and we just need to export from here.
+import { TodoContext,Todoprovider,useTodo } from "./Todocontext";
